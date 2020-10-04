56793b80edad76b44aef8768196078678db65a43
Ma~ <56109794+penguinFSS@users.noreply.github.com>
2020-10-04 16:22:03 +0200

    Update JS_fibonacci.js

--- JS_fibonacci.js
+++ JS_fibonacci.js
@@ -3,3 +3,6 @@ function fibonacci(num) {
 
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
+
+var number = window.prompt("Enter your number: ");
+alert("Fibonacci number of " + number + " is: " + fibonacci(number));
