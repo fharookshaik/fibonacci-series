5be3dc15835fa96bfae4284fea716b29e1027de9
Restyled.io <commits@restyled.io>
2020-10-05 17:06:37 +0000

    Restyled by black

--- python_fibonacci_recursion.py
+++ python_fibonacci_recursion.py
@@ -4,14 +4,14 @@
 
 def fibonacci(n):
     if n <= 0:
-        print('Wrong Input!')
+        print("Wrong Input!")
     elif n == 1:
         return 0
     elif n == 2:
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-num = int(input('Enter the nth term: '))
+num = int(input("Enter the nth term: "))
 print(fibonacci(num))
