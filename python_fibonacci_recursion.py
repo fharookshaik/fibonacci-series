6b1651262120d3635e63fdb54b87f10f9de8feb1
avk29 <66971874+avk29@users.noreply.github.com>
2020-10-05 19:23:40 +0530

    add python_fibonacci_recursion.py
    python fibonacci code via recursion

---
+++ python_fibonacci_recursion.py
@@ -0,0 +1,19 @@
+#Recursive fibonnaci code
+#this algorithm uses recursion to find the nth term in a fibonacci series
+
+def fibonacci(n):
+    
+    if n<=0:
+        print('Wrong Input!')
+        
+    elif n==1:
+        return 0
+    
+    elif n==2:
+        return 1
+      
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+num = int(input('Enter the nth term: '))
+print(fibonacci(num))
