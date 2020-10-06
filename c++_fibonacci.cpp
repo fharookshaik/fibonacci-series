ad157b5b71a5e77700c169010acb73051a589a3c
Rudransh <72294547+Red-0111@users.noreply.github.com>
2020-10-06 12:02:32 +0530

    Update c++_fibonacci.cpp

--- c++_fibonacci.cpp
+++ c++_fibonacci.cpp
@@ -2,32 +2,23 @@
 using namespace std;
 
 int main()
- {
-    int n, t1 = 0, t2 = 1, nextTerm = 0;
+{
+    int t1 = 0, t2 = 1, nextTerm = 0, n;
 
-    cout << "Enter the number of terms: ";
+    cout << "Enter a positive number: ";
     cin >> n;
 
-    cout << "Fibonacci Series: ";
+    // displays the first two terms which is always 0 and 1
+    cout << "Fibonacci Series: " << t1 << ", " << t2 << ", ";
 
-    for (int i = 1; i <= n; ++i)
+    nextTerm = t1 + t2;
+
+    while(nextTerm <= n)
     {
-        // Prints the first two terms.
-        if(i == 1)
-        {
-            cout << " " << t1;
-            continue;
-        }
-        if(i == 2)
-        {
-            cout << t2 << " ";
-            continue;
-        }
-        nextTerm = t1 + t2;
+        cout << nextTerm << ", ";
         t1 = t2;
         t2 = nextTerm;
-        
-        cout << nextTerm << " ";
+        nextTerm = t1 + t2;
     }
     return 0;
 }
