250ff1773deae13151b3d66452a6721bceea1c8d
Restyled.io <commits@restyled.io>
2020-10-06 04:45:11 +0000

    Restyled by clang-format

--- Java_fibonacci.java
+++ Java_fibonacci.java
@@ -4,22 +4,20 @@ import java.util.Scanner;
 
 public class Java_fibonacci {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 Scanner sc=new Scanner(System.in);
-         System.out.println("Enter a number of members in series: ");
-         int n=sc.nextInt();
-         int count=0;
-         int a=1,b=1;
-         System.out.print(a+" "+b+" ");
-         while(count<(n-2))
-         {
-            int c=a+b;
-            System.out.print(c+" ");
-            b=c;
-            a=b;
-            count++;
-         }
-	}
-
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number of members in series: ");
+    int n = sc.nextInt();
+    int count = 0;
+    int a = 1, b = 1;
+    System.out.print(a + " " + b + " ");
+    while (count < (n - 2)) {
+      int c = a + b;
+      System.out.print(c + " ");
+      b = c;
+      a = b;
+      count++;
+    }
+  }
 }
