e493be4d36c7ac5afb055a813d10eeda0dac8bde
KwehDev <Kwehdev@protonmail.com>
2020-10-07 22:22:38 +0300

    Added Semicolons, Removed Console output

--- typescript_fibonacci.ts
+++ typescript_fibonacci.ts
@@ -1,12 +1,12 @@
 const getFibonacciNumbers = (seriesLength: number): number[] => {
   //Will always start with 0, 1, so we can safely initialize.
-  const seriesArr: number[] = [0, 1]
+  const seriesArr: number[] = [0, 1];
 
   //We added the two initial numbers, so remove 2 from series length to compensate.
   for (let i = 1; i <= seriesLength - 2; i++) {
-    seriesArr.push(seriesArr[i - 1] + seriesArr[i])
+    seriesArr.push(seriesArr[i - 1] + seriesArr[i]);
   }
-  return seriesArr
-}
+  return seriesArr;
+};
 
-console.log(getFibonacciNumbers(50))
+getFibonacciNumbers(50);
