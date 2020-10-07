ee52fabb538cb319d6df8697020cbb9aa7f8acd5
AasthaGoyalgit <66162540+AasthaGoyalgit@users.noreply.github.com>
2020-10-07 21:37:48 +0530

    Fibonacci Series in java for any number of terms

--- Java_fibonacci.java
+++ Java_fibonacci.java
@@ -6,16 +6,18 @@ public class Java_fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 Scanner sc=new Scanner(System.in);
+         Scanner sc=new Scanner(System.in);
          System.out.println("Enter a number of members in series: ");
          int n=sc.nextInt();
          int count=0;
-         int a=1,b=1;
-         System.out.print(a+" "+b+" ");
-         while(count<(n-2))
+      
+        BigInteger a=new BigInteger("0");
+        BigInteger b=new BigInteger("1");
+        System.out.print(a+"\n "+b+"\n ");
+        while(count<(n-2))
          {
-            int c=a+b;
-            System.out.print(c+" ");
+            BigInteger c=new BigInteger(a.add(b).toString());;
+            System.out.print(c +"\n ");
             b=c;
             a=b;
             count++;
