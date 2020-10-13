375194a1c1ae5ed9e2306815b4426fb839953854
Akshaya <18wh1a1228@bvrithyderabad.edu.in>
2020-10-13 16:34:15 +0530

    Modified Fibonacci Series CPP

--- cpp_fibonacci.cpp
+++ cpp_fibonacci.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    int i, n, t1 = 0, t2 = 1, t3;
+    int i, n, t1 = 0, t2 = 1;
    cout<<"Enter the number of terms: ";
    cin>>n;
-    cout<<"Fibonacci Series: "<<" ";
+    cout<<"Fibonacci Series: "<<endl;
 
     for (i = 1; i <= n; ++i) {
-       cout<<t1<< " ";
-        t3 = t1 + t2;
+       cout<<t1<< ","<<" ";
+      int  t3 = t1 + t2;
         t1 = t2;
         t2 = t3;
     }
