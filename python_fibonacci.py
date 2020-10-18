c56cd1da23756d62b80f5850b913b48d879a4459
fharookshaik <fharookshaik.5@gmail.com>
2020-10-18 16:47:44 +0530

    Update python_fibonacci.py

--- python_fibonacci.py
+++ python_fibonacci.py
@@ -2,12 +2,13 @@ def fibonacci(num):
     num1 = 0
     num2 = 1
     series = 0
-    for i in range(num):
+    i = 0
+    while i<=num:
         print(series);
         num1 = num2;
         num2 = series;
         series = num1 + num2;
- 
+        i+= 1
  
 # running function after taking user input
 num = int(input('Enter how many numbers needed in Fibonacci series : '))
