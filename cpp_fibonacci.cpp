ce42a566b51fdec9d97135571ec3f3072b9d527b
fharook shaik <47080241+fharookshaik@users.noreply.github.com>
2020-10-21 21:03:38 +0530

    Update cpp_fibonacci.cpp

--- cpp_fibonacci.cpp
+++ cpp_fibonacci.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main() 
 {
-    int i, n, t1 = 0, t2 = 1 ;
+    int i,n;
+   unsigned long int t1 = 0, t2 = 1, t3 = 0 ;
    cout<<"Enter the number of terms: ";
    cin>>n;
     cout<<"Fibonacci Series: "<<endl;
 
     for (i = 1; i <= n; ++i) {
        cout<<t1<< ","<<" ";
-      int  t3 = t1 + t2;
+      t3 = t1 + t2;
         t1 = t2;
         t2 = t3;
     }
