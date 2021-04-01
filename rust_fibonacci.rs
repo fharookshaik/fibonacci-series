7ffb5584bb2d75786c65adc3897ccd3c7d10367e
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2021-04-01 21:15:02 +0530

    Update rust_fibonacci.rs to take user input

--- rust_fibonacci.rs
+++ rust_fibonacci.rs
@@ -1,6 +1,8 @@
-pub fn fib(n: u64) -> u64 {
-    let mut a: u64 = 0;
-    let mut b: u64 = 1;
+use std::io;
+
+fn fib(n: u128) -> u128 {
+    let mut a: u128 = 0;
+    let mut b: u128 = 1;
 
     for _ in 0..n {
         let temp = b;
@@ -8,4 +10,22 @@ pub fn fib(n: u64) -> u64 {
         a = temp;
     }
     a
+}
+
+fn main() {
+    println!("How many fibonacci terms should be printed?");
+
+    let mut n: String = String::new();
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Failed to read input");
+
+    let n: u128 = n
+        .trim()
+        .parse()
+        .expect("Please enter a valid number");
+
+    for i in 1..(n+1) {
+        println!("{}", fib(i));
+    }
 }
