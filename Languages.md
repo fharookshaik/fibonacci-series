9c45fb50edba54d3910a15748858f648846c5af4
Chiro <sachinsinghroxx@gmail.com>
2021-05-01 11:23:44 +0530

    updated languages

--- Languages.md
+++ Languages.md
@@ -17,3 +17,4 @@ Hello Coder!!
 - Kotlin
 - C++
 - TypeScript
+- MATLAB
--- matlab_fibonacci.m
+++ matlab_fibonacci.m
@@ -1,11 +1,13 @@
-n=input('Enter a number of members in series: ') //take input as n
-f= zeros(n,1);          // make an array of zeros
-f(1)=1;
-f(2)=1;
+n=input('Enter a number of members in series: ') %take input as n
+f= zeros(n,1);          %make an array of zeros
+
+f(2)=1;                 % setting second element as 1
+
+fprintf('%d %d ',f(1),f(2))
 
 for i = 3 : n
-    f(i) = f(i-1) + f(i-2);         //loop till n
-    fprintf('%d',f(i))            //display as float
+    f(i) = f(i-1) + f(i-2);         %loop till n
+    fprintf('%d ',f(i))            %display as float
     
 end 
 
