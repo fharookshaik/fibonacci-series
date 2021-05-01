414bb9f473bd61b256ca50d783435099f8fcec5a
Chiro <sachinsinghroxx@gmail.com>
2021-05-01 11:11:56 +0530

    added matlab code

---
+++ matlab_fibonacci.m
@@ -0,0 +1,16 @@
+n=input('Enter a number of members in series: ') //take input as n
+f= zeros(n,1);          // make an array of zeros
+f(1)=1;
+f(2)=1;
+
+for i = 3 : n
+    f(i) = f(i-1) + f(i-2);         //loop till n
+    
+end 
+
+fprintf('%d',f(n))            //display as float
+
+
+
+
+
