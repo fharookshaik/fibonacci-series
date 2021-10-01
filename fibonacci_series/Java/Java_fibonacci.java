99bbf02519f647c5d7a85266b740e3b83cd5ae97
Divyansh Bhardwaj <divyanshb22@gmail.com>
2021-10-01 15:35:09 +0530

    code enhancement

--- fibonacci_series/Java/Java_fibonacci.java
+++ fibonacci_series/Java/Java_fibonacci.java
@@ -1,25 +1,35 @@
 import java.util.Scanner;
 import java.math.BigInteger;
 
-public class Java_fibonacci {
+class FibonacciExampleJava {
+    public static void main(String[] args) {
+        // create an instance of the Scanner class to take an input from the user
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-         Scanner sc=new Scanner(System.in);
-         System.out.println("Enter a number of members in series: ");
-         int n=sc.nextInt();
-         int count=0;
-      
-        BigInteger a=new BigInteger("0");
-        BigInteger b=new BigInteger("1");
-        System.out.print(a+"\n "+b+"\n ");
-        while(count<(n-2))
-         {
-            BigInteger c=new BigInteger(a.add(b).toString());
-            System.out.print(c +"\n ");
-            a=b;
-            b=c;
-            count++;
-         }
-	}
+        // show a prompt to the user to input the number of terms in the series
+        System.out.println("How many terms would you like in the series?: ");
 
+        // take the input, using BigInteger to virtually store very large integer numbers
+        int numberOfTerms = scanner.nextInt();
+
+        // create the first term and the second term of the series
+        BigInteger firstTerm = BigInteger.ZERO;
+        BigInteger secondTerm = BigInteger.ONE;
+
+        // print the first two terms of the series, using printf for formatting
+        System.out.printf("%d, %d, ", firstTerm, secondTerm);
+
+        // calculate the next terms in the series using a loop
+        int termsPrinted = 2;
+        while (termsPrinted < numberOfTerms) {
+            BigInteger nextTerm = firstTerm.add(secondTerm);
+            // print the next term in the series
+            System.out.printf("%d%s", nextTerm, (termsPrinted != numberOfTerms - 1 ? ", " : "."));
+            // update the firstTerm and the secondTerm variables
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+            // update the number of terms printed in the series
+            termsPrinted++;
+        }
+    }
 }
