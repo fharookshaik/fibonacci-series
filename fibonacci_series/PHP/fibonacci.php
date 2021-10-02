7bfdf5e55b8892846f82ae026fcaafcc7bf0c1d4
Nihal Ahmad <nehalhmd14@gmail.com>
2021-10-02 15:45:41 +0530

    Added fibonacci series recursive code in php langauge

---
+++ fibonacci_series/PHP/fibonacci.php
@@ -0,0 +1,20 @@
+    <?php  
+    /* Print fiboancci series upto 12 elements. */  
+    $num = 12;  
+    echo "<h3>Fibonacci series using recursive function:</h3>";  
+    echo "\n";  
+    /* Recursive function for fibonacci series. */  
+    function series($num){  
+        if($num == 0){  
+        return 0;  
+        }else if( $num == 1){  
+    return 1;  
+    }  else {  
+    return (series($num-1) + series($num-2));  
+    }   
+    }  
+    /* Call Function. */  
+    for ($i = 0; $i < $num; $i++){  
+    echo series($i);  
+    echo "\n";  
+    }  
