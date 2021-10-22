2a44c1a013fda7a8afe065889858838c01d4ded1
sarthakbhatia05 <67466428+sarthakbhatia05@users.noreply.github.com>
2021-10-22 23:21:13 +0530

    Update R_fibonacci.r

--- fibonacci_series/R/R_fibonacci.r
+++ fibonacci_series/R/R_fibonacci.r
@@ -1,5 +1,27 @@
-#first 10 fibonacci numbers
-Fibonacci <- numeric(10)
-Fibonacci[1] <- Fibonacci[2] <- 1
-for (i in 3:10) Fibonacci[i] <- Fibonacci[i - 2] + Fibonacci[i - 1]
-print(Fibonacci)
+# take input from the user
+n = as.integer(readline(prompt="Enter Number of Terms "))
+# first two terms
+a = 0
+b = 1
+count = 2
+# check if the number of terms is valid
+if(n <= 0) {
+print("Plese enter a positive integer")
+} else {
+if(n == 1) {
+print("Fibonacci sequence:")
+print(a)
+} else {
+print("Fibonacci sequence:")
+print(a)
+print(b)
+while(count < n) {
+c = a + b
+print(c)
+# update values
+a = b
+b = c
+count = count + 1
+}
+}
+}
