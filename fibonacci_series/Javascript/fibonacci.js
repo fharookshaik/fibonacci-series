5f356b02f2ec9fdf58041732c95c1ab05da5670f
Ingenious <76046349+Ingenious-c0der@users.noreply.github.com>
2021-10-25 10:42:26 +0530

    Update fibonacci.js

--- fibonacci_series/Javascript/fibonacci.js
+++ fibonacci_series/Javascript/fibonacci.js
@@ -1,7 +1,12 @@
+
+/*codacy does not allow console logging , but there is no other alternative for this since I am not running it on any browser.
+If at all this needs to be passed as 0 issues on codacy , I will need to remove all the console.log() and then there will be simply no 
+output although the function will run all fine.*/
+
 function fibonacci(n){
 	let n1 = 0 , n2 =1 ,next ;
 	next = n1+n2;
-	console.log("fibonacci series :")
+	console.log("fibonacci series :");
 	console.log(n1);
 	console.log(n2);
 	while (next<n){
