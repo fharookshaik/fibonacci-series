45be336c751b49161cfab53f4bee8faf799b663b
chgara <57034271+chgara@users.noreply.github.com>
2021-10-25 10:14:02 +0200

    Update fibonacci_series/Typescript/typescript_recursive_fibonacci.ts
    Co-authored-by: fharook shaik <47080241+fharookshaik@users.noreply.github.com>

--- fibonacci_series/Typescript/typescript_recursive_fibonacci.ts
+++ fibonacci_series/Typescript/typescript_recursive_fibonacci.ts
@@ -40,7 +40,7 @@ class Fibonacci {
       return this.fibonacci(n - 1) + this.fibonacci(n - 2);
     } else if (n === 1) {return 1;}
     else if (n === 0) {return 0;}
-    else return -1;
+    else {return -1;}
   }
 
   /**
