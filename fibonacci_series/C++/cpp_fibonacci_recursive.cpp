fdddf575d232ec1f4bbff46cae82cf5e9300ad6e
Ingenious <76046349+Ingenious-c0der@users.noreply.github.com>
2021-10-27 18:29:31 +0530

    Update cpp_fibonacci_recursive.cpp

--- fibonacci_series/C++/cpp_fibonacci_recursive.cpp
+++ fibonacci_series/C++/cpp_fibonacci_recursive.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 
-const int fibonacci(unsigned long long int n){
+const int fibonacci( long int n){
     if(n<=0) return -1;
     if(n==1) return 0;
     if(n==2)return 1;
@@ -9,7 +9,7 @@ const int fibonacci(unsigned long long int n){
 }
 
 int main()
-{   unsigned long long int  n; 
+{    long int  n; 
     std::cout<<"Enter the nth fibonacci number which needs to be found out :";
     std::cin >>  n ;
     std::cout<< fibonacci(n) << std::endl;
