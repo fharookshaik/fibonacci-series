e51a8e41d9d530d86e8dd57ca4dc81226305b415
shravan20 <mrshravankumarb@gmail.com>
2021-10-29 19:08:56 +0530

    [fix] refactor code as per codacy review

--- javascript_fibonacci.js
+++ javascript_fibonacci.js
@@ -1,11 +1,11 @@
 const fibo = (n, memo = {}) => memo[n] || (n <= 2 ? 1 : (memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo)));
 
-const readline = require('readline').createInterface({
+const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout
 });
  
-readline.question('Enter the Nth term: ', number => {
+readline.question("Enter the Nth term: ", (number) => {
 	console.log(fibo(number));
 	readline.close();
 });
