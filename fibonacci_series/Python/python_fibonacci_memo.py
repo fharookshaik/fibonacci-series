c4cb96bfacff9c3f282a7e8bdd27c0ca73ce7e97
AntroSafin <85235476+AntroSafin@users.noreply.github.com>
2021-12-10 11:40:54 +0530

    Update python_fibonacci_memo.py

--- fibonacci_series/Python/python_fibonacci_memo.py
+++ fibonacci_series/Python/python_fibonacci_memo.py
@@ -1,14 +1,14 @@
 #python fibonacci using memoization which execute faster than normal recursion
 
 def fibonacci(n,memo={}):
-    if(n < 0):
-      return "Wrong Input"
-    if(n == 0):
-      return 0
     if(n in memo):
       return memo[n]
-    if(n == 1 or n == 2):
+    elif(n == 0):
+      return 0
+    elif(n == 1 or n == 2):
       return 1
+    else:
+      return "Wrong Input"
     memo[n]=fibonacci(n - 1,memo) + fibonacci(n - 2,memo)
     return memo[n]
 
