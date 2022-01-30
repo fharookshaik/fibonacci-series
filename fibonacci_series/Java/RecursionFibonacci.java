b8a19f669847f8aa2d6e1a4f07a738c1345493e5
saugat <saugatshrestha602@gmail.com>
2022-01-30 22:59:31 +0545

    fibonacci java recursion

---
+++ fibonacci_series/Java/RecursionFibonacci.java
@@ -0,0 +1,23 @@
+import java.util.scanner;
+
+public class RecursionFibonacci{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        // show a prompt to the user to input the number of terms in the series
+        System.out.println("How many terms would you like in the series?: ");
+
+        // take the input, using BigInteger to virtually store very large integer numbers
+        int n = sc.nextInt();
+        int fib = fib(n);
+        System.out.println(n);
+
+
+    }
+    public static int fib(int n){
+        if(n<1){
+            return n;
+        }
+        return fib(n-1) + fib(n+2);
+    }
+}
