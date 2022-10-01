c71b98908e1ab42aed8efad5843763ae8ff411fd
Gautam Shrotriya <shrotriyagautam101@gmail.com>
2022-10-01 21:02:07 +0530

    Added JavaFibonacciRecursive.java with correct naming conventions

--- fibonacci_series/Java/javaFibonacciRecursive.java
+++ fibonacci_series/Java/javaFibonacciRecursive.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class javaFibonacciRecursive {
+public class JavaFibonacciRecursive {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
