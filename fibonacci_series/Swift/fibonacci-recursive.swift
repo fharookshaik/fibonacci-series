d3939bf87dec5c8f8bb89251f3865318c952cd67
Aaron Philip <aaronphilip2003@gmail.com>
2022-10-02 23:37:52 +0530

    Implemented fibonacci series in Swift

--- fibonacci_series/Swift/fibonacci-recursive.swift
+++ fibonacci_series/Swift/fibonacci-recursive.swift
@@ -1,18 +1,14 @@
-func fibonacci(n:Int)->Int{
-	if (n==0) || (n==1){
-		return n  //In case n==0 or n==1, we don't always print the first two elements
-	}
-	else
-	{
-		return fibonacci(n:n-1) + fibonacci(n:n-2)
-	}
+func fibonacci(num: Int) -> Int {
+	if (num==0) || (num==1) {
+		return num }  //In case n==0 or n==1, we don't always print the first two elements 
+	else {
+		return fibonacci(num: num-1) + fibonacci(num: num-2) }
 }
 
 print("Enter the number of elements:")
 let number = readLine()
-var num=Int(number!)
-var i=0
-while(num!>=i){
-	print(fibonacci(n:i))
-	i+=1
-}
+var num_int=Int(number!)
+var iterator=0
+while(num_int!>=iterator) {
+	print(fibonacci(num: iterator))
+	iterator+=1 }
