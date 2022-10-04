aad56048c6a4d63da87da60a2205e5671722ccc1
MD MOQADDAS <69861558+MDMOQADDAS@users.noreply.github.com>
2022-10-04 15:04:58 +0530

    Changed Required things

--- fibonacci_series/Python/Fibonacci_dict.py
+++ fibonacci_series/Python/Fibonacci_dict.py
@@ -1,22 +1,10 @@
 def fib( n):
-        if n == 0:
-            return 0
-
-        elif n == 1:
-            return 1
-
-        else:
-            res = {
-                0: 0,
-                1: 1
-            }
-
-            for i in range(1, n):
-                temp = res[i] + res[i-1]
-                res[i+1] = temp
-                
-                
-
-            return list(res.values())[-1]
-
-print(fib(8))
+	if n == 0: return 0
+	elif n == 1: return 1
+	else: res = { 0: 0,1: 1}
+	for i in range(1, n):
+		temp = res[i] + res[i-1]
+		res[i+1] = temp
+	return list(res.values())[-1]
+n=int(input())
+fib(n)
