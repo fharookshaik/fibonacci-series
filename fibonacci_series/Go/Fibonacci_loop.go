11e4d0178394134f5d808a36f9bda2d8809758c2
nilesh05apr <nk8588338@gmail.com>
2022-10-06 01:53:17 +0530

    add: comments

--- fibonacci_series/Go/Fibonacci_loop.go
+++ fibonacci_series/Go/Fibonacci_loop.go
@@ -1,8 +1,10 @@
 package main
 
+//importing fmt package for formatted I/O.
 import ("fmt")
 
-
+//fib array stores previously calculated values of the series
+//Time Complexity: O(N) 
 func fibonnaci(x int) []int {
 	fib := []int{0,1}
 	for i := 2; i <= x; i++ {
@@ -14,8 +16,10 @@ func fibonnaci(x int) []int {
 
 func main() {
 	var num int
+	//C style I/O using scanf and printf
 	fmt.Printf("Enter a number: \n")
 	fmt.Scanf("%d",&num)
+	//calculating the fibonnaci number
 	res := fibonnaci(num)
 	fmt.Println(res)
 }
