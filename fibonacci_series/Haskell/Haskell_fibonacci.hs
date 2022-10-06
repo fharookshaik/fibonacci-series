af442a744a8344cb03193d5996f1128e56c555c3
Eduardo Freitas de Carvalho <eduardo.carvalho@pier.digital>
2022-10-06 18:02:18 -0300

    Improve Haskell fibonacci

--- fibonacci_series/Haskell/Haskell_fibonacci.hs
+++ fibonacci_series/Haskell/Haskell_fibonacci.hs
@@ -1,5 +1,7 @@
+fibonacci :: [Int]
+fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
+
 main = do
   putStrLn "Enter the number os elements:"
   input <- getLine
-  let fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
   print (take (read input :: Int) fibonacci)
