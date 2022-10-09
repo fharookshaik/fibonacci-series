e8252a680d23eac3340bd75be4bd5f9b1b96b6f3
das-amit <91092039+das-amit@users.noreply.github.com>
2022-10-10 02:18:46 +0530

    Added the Java Fibonacci solution using recursion

---
+++ fibonacci_series/Java/FibonacciRecursion.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class FibonacciRecursion {
+    private static long[] fibonacciCache;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("How many terms would you like in the series?:");
+        int numberOfTerms = in.nextInt();
+        fibonacciCache = new long[numberOfTerms + 1];
+        for (int i = 0; i < numberOfTerms; i++){
+            System.out.println(fibonacci(i) +" ");
+        }
+      in.close();
+    }
+    private static long fibonacci(int n) {
+        if(n <= 1){
+            return n;
+        }
+        if(fibonacciCache[n] != 0){
+            return fibonacciCache[n];
+        }
+        long nthFibonacciNumber = (fibonacci(n - 1) + fibonacci(n - 2));
+        fibonacciCache[n] = nthFibonacciNumber;
+        return nthFibonacciNumber;
+    }
+}
