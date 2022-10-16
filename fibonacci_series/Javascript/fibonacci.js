6e3c776906ae1013e04e5bda674dbcbd2d18e004
aman kothiyal <amankothiyal474@gmail.com>
2022-10-16 13:05:17 +0530

    added a new program for fibo series

--- fibonacci_series/Javascript/fibonacci.js
+++ fibonacci_series/Javascript/fibonacci.js
@@ -1,10 +1,10 @@
 // program to generate fibonacci series up to a certain number
 
 // take input from the user
-const number = parseInt(prompt('Enter a positive number: '));
+const number = parseInt(prompt("Enter a positive number: "));
 let n1 = 0, n2 = 1, nextTerm;
 
-console.log('Fibonacci Series:');
+console.log("Fibonacci Series:");
 console.log(n1); // print 0
 console.log(n2); // print 1
 
