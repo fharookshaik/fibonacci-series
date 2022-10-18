fb54f06ce554d7df9b6b56c8dbb0f37467826d84
Ramlakhan12 <ramlakhan78788@gmail.com>
2022-10-19 01:19:04 +0530

    fibonacci_series code in php

--- PHP_fibonacci.php
+++ PHP_fibonacci.php
@@ -3,7 +3,7 @@
 $num = 0;  
 $n1 = 0;  
 $n2 = 1;  
-echo "<h3>Fibonacci series for first 12 numbers: </h3>";  
+echo "<h3>Fibonacci series for first 100 terms: </h3>";  
 echo "\n";  
 echo $n1.' '.$n2.' ';  
 while ($num < 100 )  
