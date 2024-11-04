dd29978a3792ee991db63708fd289bf642dbda09
Tomislav <tomislav.tomaskovic8@hotmail.com>
2024-11-04 10:35:04 +0100

    Add Node.js implementation of Fibonacci in nested folder

---
+++ fibonacci_series/Node.js/nodejs_fibonacci_iteration.js
@@ -0,0 +1,15 @@
+fibonacciIterative = (n) => {
+  if (n <= 1) return n;
+
+  let prev = 0;
+  let curr = 1;
+
+  for (let i = 2; i <= n; i++) {
+    let temp = curr;
+    curr = curr + prev;
+    prev = temp;
+  }
+  return curr;
+};
+
+console.log(fibonacciIterative(9));
---
+++ fibonacci_series/Node.js/nodejs_fibonacci_recursive.js
@@ -0,0 +1,6 @@
+const fibonacciRecursive = (n) => {
+  if (n <= 1) return n;
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+};
+
+console.log(fibonacciRecursive(7));
