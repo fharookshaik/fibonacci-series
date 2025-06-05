17d281a6cb0fce358f70959124b0c57b65d12e65
Osifalujoi1 <osifalujoi1@mymail.nku.edu>
2025-06-05 12:22:54 -0400

    add JavaFibonacciRecursion.java

---
+++ fibonacci_series/Java/JavaFibonacciRecursion.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class JavaFibonacciRecursion {
+
+    public static int fibonacci(int n) {
+        if (n <= 1){
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    } 
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 0; i < n; i++){
+            System.out.print(fibonacci(i) + " ");
+        }
+        
+    }
+}
